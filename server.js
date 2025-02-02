4f1364dd7f368fde79f58bbd68fa187ee2b67896
Paul Chukwujekwu <paulchukwu2001@gmail.com>
2025-02-02 04:37:58 +0100

    updated cors

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors({
-    "origin": 'http://127.0.0.1:5500/',
-    "credentials": true
+    origin: 'http://127.0.0.1:5500',
+    credentials: true
 }));
 
 app.use(express.json());
