a042470f664b17fed447ca270e791848a79cc3d8
Paul Chukwujekwu <paulchukwu2001@gmail.com>
2025-02-02 03:47:25 +0100

    third commit

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,24 @@
-document.addEventlistener() => {
+document.addEventlistener('DOMContentLoaded', () => {
+    const tableBody = document.getElementById('data-table-body')
+    const fetchDataAnddisplay = async () => {
+        try {
 
-}
+            const response = await fetch('https://restful-library-api.onrender.com/users', {})
+
+            const data = await response.json()
+            const rows = data.map(post => {
+                return `
+                <tr>
+                <td>${post.name}</td>
+                <td>${post.username}</td>
+                <td>${post.email}</td>
+                </tr>
+                `
+            }).join('')
+            tableBody.innerHTML = rows
+        } catch (err) {
+            console.log('Error loading data', err)
+            tableBody.innerHTML = `<tr><td colspan="3" class="text-center text-danger">Error Fetching Data</td></tr>`
+        }
+        fetchDataAnddisplay()
+    });
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors({
-    "origin": '',
+    "origin": 'http://127.0.0.1:5500/',
     "credentials": true
 }));
 
