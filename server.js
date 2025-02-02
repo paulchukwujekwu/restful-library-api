ba37634a33d0680e973bbb681af2e4fcafc802c9
Paul Chukwujekwu <paulchukwu2001@gmail.com>
2025-02-02 06:17:37 +0100

    reduced the time limit

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const app = express();
 
 // Set up rate limiting
 const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per windowMs
+    windowMs: 1 * 60 * 1000, // 15 minutes
+    max: 1, // Limit each IP to 100 requests per windowMs
     message: "Too many requests from this IP, please try again later."
 });
 
